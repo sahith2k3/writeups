bf3f502353dc9d98be3d57186d3d7c343583e009
Sahith Chowdary M <48013334+sahith2k3@users.noreply.github.com>
2021-01-22 00:29:05 +0530

    Update ENCODING_Base64.py

--- cryptography/cryptohack/GENERAL/ENCODING_Base64.py
+++ cryptography/cryptohack/GENERAL/ENCODING_Base64.py
@@ -1,3 +1,11 @@
+
+this is the writeup for BASE64 task in GENERAL/ENCODING from cryptohack.org
+
+I simply imported the base64 module to use b64encode() method to convert bytes to base64
+
+
+python code:
+```
 import base64
 
 ciphert_byte = bytes.fromhex("72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf")
@@ -5,3 +13,4 @@ ciphert_byte = bytes.fromhex("72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269
 plaint = base64.b64encode(ciphert_byte)
 
 print(plaint)
+```
